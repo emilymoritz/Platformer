5c4966291ad3fcb1537ec65a3b1c5436ec2be916
Emily Moritz <fullmetal1104@gmail.com>
2019-07-15 21:05:22 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    public float speed;
+    public float jumpforce;
+
+    void Start() 
+    {
+        rb2d = GetComponent<Rigidbody2D>(); 
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void FixedUpdate()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(movehorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
